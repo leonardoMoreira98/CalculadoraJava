85b44df7efb67d14a3f033462b35a09284e13528
Leonardo Moreira <89355249+leonardoMoreira98@users.noreply.github.com>
2022-04-24 21:19:12 -0300

    Update ExemploPrograma.java

--- CalculadoraPoo/src/programa/ExemploPrograma.java
+++ CalculadoraPoo/src/programa/ExemploPrograma.java
@@ -1,24 +1,42 @@
-package programa;
+package classes;
 
-import java.util.Scanner;
+import interfaces.ICalculadora;
 
-public class ExemploPrograma {	
+public class Multiplicacao implements ICalculadora {
 	
-	public static void main(String args[]) {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Digite a operação que deseja ? [1 - Teste]");
-		int opc = scan.nextInt(); // le a entrada do terminal
-		
-//		Exemplo das chamadas que deverão ser feitas
-//		ICalculadora calculadora = new Soma();
-//		ICalculadora calculadora = new Subtracao();
-//		ICalculadora calculadora = new Divisao();
-//		ICalculadora calculadora = new Multiplicacao();
-//		
-//		calculadora.calcular(null);
-		
-		scan.close();
-		
+	public Integer calcula(Integer n1, Integer n2) {
+	   return n1 * n2;
+	 }
+
+	 public Float calcula(Float n1, Float n2) {
+	   return n1 * n2;
+	 }
+
+	 public Double calcula(Double n1, Double n2) {
+	   return n1 * n2;
+	 }
+
+	 public Integer calcula(Integer[] numbers) {
+	   Integer multi = 1;
+	for (int i = 0; i < numbers.length; i++) {
+	multi *= numbers[i];
 	}
-}
+	return multi;
+	 }
+
+	 public Float calcula(Float[] numbers) {
+	 Float multi = 1F;
+	for (int i = 0; i < numbers.length; i++) {
+	multi *= numbers[i];
+	}
+	return multi;
+	 }
+
+	 public Double calcula(Double[] numbers) {
+	 Double multi = 1D;
+	for (int i = 0; i < numbers.length; i++) {
+	multi *= numbers[i];
+	}
+	return multi;
+	 }
+	}
